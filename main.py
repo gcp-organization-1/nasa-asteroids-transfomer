3b9d80de48fd2ec61244e011c5bfccd4280f6fb4
Luka <luka.akhalkatsi@makingscience.com>
2026-02-17 17:18:07 +0400

    update main.py

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from transformer.asteroids_transformer import AsteroidsTransformer
+from gcp.gcs import GCSClient
 
 def main(request):
+    request_json = request.get_json(silent=True)
+    file_path = request_json.get("file_path")
+
+    if not file_path:
+        return {"error": "file_path is required"}, 400
+
     try:
-        request_json = request.get_json(silent=True)
-        if request_json is None:
-            return {"error": "Request body must be JSON"}, 400
+        gcs = GCSClient()
+        raw_data = gcs.download_blob(file_path)
 
         transformer = AsteroidsTransformer()
-        df = transformer.transform_asteroids_data(request_json)
-        to_str = df.to_string()
-        return to_str
+        df = transformer.transform_asteroids_data(raw_data)
+
+        output_path = "processed/data.csv"
+
+        gcs.upload_transformed_data_to_gcs(df, output_path)
+
+        return {"message": "Transform complete", "file_path": output_path}
 
     except Exception as e:
         return {"error": str(e)}, 500
